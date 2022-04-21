8f78bcb49e1baa3fb804f64d53b4b95a35e04bb8
Vitor Amaral <65573808+Vitoramaraldsa@users.noreply.github.com>
2022-04-21 20:51:50 -0300

    Versão inicial do projeto
    Envio de Emails + salvamento no banco de dados Postgree

---
+++ ms-email/src/main/java/com/ms/email/controllers/EmailController.java
@@ -0,0 +1,34 @@
+package com.ms.email.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ms.email.dtos.EmailDto;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
+
+@RestController
+public class EmailController {
+	
+	@Autowired
+	EmailService emailService;
+	
+	//endpoint para enviar o email
+	@PostMapping("/sending-email")
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto) {
+        EmailModel emailModel = new EmailModel();
+       
+        //Copia as propriedades de uma classe para a outra
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+    }
+
+}
